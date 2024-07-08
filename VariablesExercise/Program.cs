08b522b951a4f362d78892c250406fa904f51c8c
Yash Bajaj <yash@bajajmd.net>
2024-07-08 16:20:09 -0400

    Wrote the story with the variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace VariablesExercise
+﻿// declare and initialize variables
+string name = "Megatron";
+int age = 38;
+char teamInitial = 'D';
+bool highschoolGrad = false;
+decimal currentSalary = 500.50m;
+
+string gradStatus;
+
+if (highschoolGrad == false)
+   gradStatus = "not";
+else
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+   gradStatus = "indeed";
 }
+
+
+Console.WriteLine($"My name is {name}, leader of team {teamInitial} for Decepticon. I am {age} and am {gradStatus} a high school grad. I make ${currentSalary} an hour!");
